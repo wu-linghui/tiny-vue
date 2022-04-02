841d58e6d5303f8bf058c6a84102249047977d4c
wu-linghui <1540996580@qq.com>
2022-04-03 00:17:48 +0800

    feat:添加effect函数的stop、onStop功能；并完善相关单元测试代码

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,13 +1,34 @@
+import { extend } from "./shared";
+
 class ReactiveEffect {
     private _fn: any;
-    constructor (fn) {
-        this._fn = fn;
+    public deps: any;
+    active = true;
+    onStop?: () => void;
+    public scheduler: Function | undefined;
+    constructor (fn, scheduler?: Function) {
+        this._fn = fn;        
+        this.scheduler = scheduler;
+        this.deps = [];
     }
 
     run () {
         activeEffect = this;
         return this._fn();        
     }
+
+    stop () {
+        if (!this.active) return;
+        clearUpDeps(this);
+        if (this.onStop) this.onStop();
+        this.active = false;
+    }
+}
+
+function clearUpDeps(effect) {    
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect);
+    })
 }
 const targetMap = new Map();
 export function track (target, key) {
@@ -23,20 +44,31 @@ export function track (target, key) {
         dep = new Set();
         depsMap.set(key, dep)
     }
+    if (!activeEffect) return;
     dep.add(activeEffect);
+    activeEffect.deps.push(dep);
 }
 
 export function trigger (target, key) {
     let depsMap = targetMap.get(target);
     let dep = depsMap.get(key);
     for (const effect of dep) {
-        effect.run()
+        effect.scheduler ? effect.scheduler() : effect.run();
     }
 }
 
 let activeEffect;
-export function effect (fn) {
-    const _effect = new ReactiveEffect(fn);
+export function effect (fn, options: any = {})  {
+    const _effect = new ReactiveEffect(fn, options.scheduler);
+    // _effect.onStop = options.onStop;
+    // Object.assign(_effect, options);
+    extend(_effect, options);
     _effect.run();
-    return _effect.run.bind(_effect);
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
+}
+
+export function stop (runner) {
+    runner.effect.stop();
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from "../effect";
+import { effect, stop } from "../effect";
 import { reactive } from "../reactive"
 
 describe("effect", () => {
@@ -29,5 +29,66 @@ describe("effect", () => {
         const r = runner();
         expect(foo).toBe(12);
         expect(r).toBe("foo");
+    })
+    /* 
+        1.通过effect的第二个参数给定一个scheduler的fn
+        2.effect第一次执行的时候还会运行第一个参数的fn
+        3.当响应式对象set update时不会执行effect第一个参数的fn而是执行scheduler
+        4.如果说当执行runner的时候，会再次执行effect第一个参数的fn
+     */
+
+    it("scheduler", () => {
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+        const obj = reactive({foo: 1});
+        const runner = effect(
+            () => {
+                dummy = obj.foo;
+            },
+            {scheduler}
+        );
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        expect(dummy).toBe(1);
+        run();
+        expect(dummy).toBe(2);
+    })
+
+    it("stop", () => {
+        let dummy;
+        const obj = reactive({prop: 1});
+        const runner = effect(() => {
+            dummy = obj.prop;
+        });
+        obj.prop = 2;
+        expect(dummy).toBe(2);
+        stop(runner);
+        obj.prop = 3;
+        expect(dummy).toBe(2);
+        runner();
+        expect(dummy).toBe(3);
+    })
+
+    it("onStop", () => {
+        const obj = reactive({
+            foo: 1
+        });
+        const onStop = jest.fn();
+        let dummy;
+        const runner = effect(
+            () => {
+                dummy = obj.foo;
+            },
+            {
+                onStop
+            }
+        );
+        stop(runner);
+        expect(onStop).toBeCalledTimes(1);
     })
 })
