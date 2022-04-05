f9b4da44c6a640b48ba8d7e093243a8b2fd2183b
wu-linghui <1540996580@qq.com>
2022-04-05 21:10:52 +0800

    feat:添加isRef、unRef、proxyRefs功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,11 +1,13 @@
 import { isTracking, trackEffects, triggerEffects } from "./effect";
 import { reactive } from "./reactive";
 import { hasChange, isObject } from "./shared";
+import { ObjectFlags } from "./shared/enum";
 
 class RefImpl {
     private _value: any;
     private _rawValue: any;
     public dep;
+    public _v_isRef: boolean =  true;
     constructor (value) {
         this.dep = new Set();
         convert(this, value);
@@ -25,6 +27,31 @@ class RefImpl {
 
 export function ref (value) {  
     return new RefImpl(value);
+}
+
+export function isRef (value) {
+    return !!value[ObjectFlags.IS_REF];
+}
+
+export function unRef (ref) {
+    return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRefs(objectWithRef) {
+    return new Proxy(objectWithRef, {
+        get (target, key) {
+            /* get -> age(ref) 那么就给它返回.value
+               不是ref -> value
+             */
+            return unRef(Reflect.get(target, key));
+        },
+
+        set (target, key, value) {
+            /* set -> 被修改的值为ref则替换掉.value并且新值不能为ref类型 */
+            if (isRef(target[key]) && !isRef(value)) return target[key].value = value;
+            return Reflect.set(target, key, value);
+        }
+    })
 }
 
 function convert (_this, value) {
--- src/reactivity/shared/enum.ts
+++ src/reactivity/shared/enum.ts
@@ -1,4 +1,5 @@
 export const enum ObjectFlags {
     IS_REACTIVE = "_v_isReactive",
     IS_READONLY = "_v_isReadonly",
+    IS_REF = "_v_isRef",
 }
