030f8a9a2fd32bb90e5294946e98bffa6ab03851
wu-linghui <1540996580@qq.com>
2022-04-17 23:56:52 +0800

    test:更新parse标签元素单元测试案例

--- src/compiler-core/test/parse.spec.ts
+++ src/compiler-core/test/parse.spec.ts
@@ -5,13 +5,22 @@ describe("Parse", () => {
     describe("interpolation", () => {
         test("simple interpolation", () => {
             const ast = baseParse("{{ msg }}");
-            console.log(ast.children[0])
             expect(ast.children[0]).toStrictEqual({
                 type: NodeType.INTERPOLATION,
                 content: {
                     type: NodeType.SIMPLE_EXPRESSION,
                     content: "msg"
                 }
+            })
+        })
+    })
+
+    describe("element", () => {
+        test("simple element div", () => {
+            const ast = baseParse("<div></div>");
+            expect(ast.children[0]).toStrictEqual({
+                type: NodeType.ELEMENT,
+                tag: "div"
             })
         })
     })
